26d280e8d51e09a8f82e788c66bbb4751b3da69e
Harsh3006 <69070307+Harsh3006@users.noreply.github.com>
2022-02-02 14:27:48 +0530

    Update settings.py

--- EpicInfluencers/settings.py
+++ EpicInfluencers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1jmbfhmli$p8pd43d_#otd4z4wqb3q=bb-bl9$roe!=55w=j('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
